29676d503f5a299d71ffaa1c54bb3365a898147b
chendrix <chendrix1123@gmail.com>
2022-01-15 11:07:38 -0500

    client redone

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,3 @@
-//import dataService from './services/data'
-
-const docId = '1YvznM3U5FS6SnirNS_JyLHElkD4FlqVJu_qz_-NIhWg'
-const sheetNames = ['Players', 'Draft']
-
-// const data = await dataService.getDataFromSheets(docId, sheetNames);
-
-const App = () => {
-  // const json = JSON.stringify(data);
-  return <div className="App">hello</div>
+export default function App () {
+  return <div className="App">hello world</div>
 }
-
-export default App
