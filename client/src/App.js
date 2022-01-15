1b787417ab2114ce89f28fa138c7dd770dfce6c2
chendrix <chendrix1123@gmail.com>
2022-01-15 13:15:29 -0500

    appbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,28 @@
-export default function App () {
-  return <div className="App">hello world</div>
+import './App.css'
+import Navigation from './Components/Navigation'
+import {Container} from '@material-ui/core'
+
+import {ThemeProvider} from '@material-ui/styles'
+import {createTheme} from '@material-ui/core/styles'
+import {grey, red} from '@material-ui/core/colors'
+
+export default function App() {
+  const theme = createTheme({
+    palette: {
+      secondary: {main: red[800]},
+      primary: {main: grey[800]}
+    },
+    typography: {
+      fontFamily: ['"Lato"', 'sans-serif'].join(',')
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container className="App">
+        <Navigation />
+        <div>hello world</div>
+      </Container>
+    </ThemeProvider>
+  )
 }
---
+++ client/src/Components/Navigation.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import {AppBar, Toolbar, Button, Typography} from '@material-ui/core'
+import {Link} from 'react-router-dom'
+
+export default function Navigation() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <img src="img/mit-logo.svg" alt="logo" style={{maxWidth:50, paddingRight:10}} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}
+        >
+          MIT Fantasy Baseball
+        </Typography>
+        <Button color="secondary" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/draft">
+          Draft
+        </Button>
+        <Button color="inherit" component={Link} to="/keepers">
+          Keepers
+        </Button>
+        <Button color="inherit" component={Link} to="/stats">
+          Stats
+        </Button>
+        <Button color="inherit" component={Link} to="/history">
+          History
+        </Button>
+        <Button color="inherit" component={Link} to="/rules">
+          Rules
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
