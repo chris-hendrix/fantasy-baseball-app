5595a16a22aa2451ede13ef12d613a9edea1404a
chendrix <chendrix1123@gmail.com>
2022-01-15 19:45:14 -0500

    react table added

---
+++ client/src/components/ReactTable.js
@@ -0,0 +1,43 @@
+import {Table, TableBody, TableCell, TableHead,TableRow} from '@material-ui/core'
+import { useTable } from 'react-table'
+
+export default function ReactTable({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <Table {...getTableProps()}>
+      <TableHead>
+        {headerGroups.map(headerGroup => (
+          <TableRow {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <TableCell {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableHead>
+      <TableBody>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <TableRow {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <TableCell {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
+  )
+}
--- client/src/components/pages/DraftPage.js
+++ client/src/components/pages/DraftPage.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
+import {Container} from '@material-ui/core'
+import ReactTable from '../ReactTable'
 import {getDraftData} from '../../reducers/dataReducer'
 
 export default function HomePage() {
-  const draft = useSelector(state => state.data)
+  const draftTableData = useSelector(state => state.data.draft.Draft)
   const dispatch = useDispatch()
   useEffect(() => dispatch(getDraftData()), [dispatch])
-  console.log(draft)
-  return <div>Draft</div>
+  console.log(draftTableData)
+  return (
+    <Container>
+      {draftTableData && <ReactTable {...draftTableData} />}
+    </Container>
+  )
 }
