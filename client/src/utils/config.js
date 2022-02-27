32a9ec0514f97dc92c92524a0df78346f1dfde3f
chendrix <chendrix1123@gmail.com>
2022-02-27 10:06:23 -0500

    fix config 2

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,11 +3,11 @@ const EMAILJS_SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID
 const EMAILJS_TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
 const EMAILJS_USER_ID = process.env.REACT_APP_EMAILJS_USER_ID
 
-export default function config () {
-  return {
-    BACKEND_URL,
-    EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPLATE_ID,
-    EMAILJS_USER_ID
-  }
+const config = {
+  BACKEND_URL,
+  EMAILJS_SERVICE_ID,
+  EMAILJS_TEMPLATE_ID,
+  EMAILJS_USER_ID
 }
+
+export default config
