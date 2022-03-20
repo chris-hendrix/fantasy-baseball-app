773b20cf8adccfd9a76b816b6cae71f30537a94d
chendrix <chendrix1123@gmail.com>
2022-03-20 10:53:58 -0400

    tweak draft page

--- client/src/components/pages/DraftPage.js
+++ client/src/components/pages/DraftPage.js
@@ -1,44 +1,42 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Divider } from '@mui/material'
 
 import MuiTable from '../tables/MuiTable'
 import { defaultColumnOptions } from '../tables/TableFilters'
 
 export default function DraftPage() {
-  const draftTableData = useSelector(state => state.draft.tables.Draft)
-  const playerTableData = useSelector(state => state.draft.tables.Players)
-  const ownerDraftTableData = useSelector(state => state.draft.tables.OwnerDraft)
+  const draftTableData = useSelector(({ draft: { tables } }) => tables.Draft)
+  const playerTableData = useSelector(({ draft: { tables } }) => tables.Players)
+  const ownerDraftTableData = useSelector(({ draft: { tables } }) => tables.OwnerDraft)
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Typography variant="h4" align="center">Draft</Typography>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={12}>
-          <Typography variant="h6">Owner Draft Info</Typography>
-          {ownerDraftTableData && <MuiTable defaultPageSize={10} {...ownerDraftTableData} />}
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h6">Draft Board</Typography>
-          {draftTableData && (
-            <MuiTable
-              defaultPageSize={220}
-              columnOptions={defaultColumnOptions}
-              {...draftTableData}
-            />
-          )}
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h6">Player Pool</Typography>
-          {playerTableData && (
-            <MuiTable
-              defaultPageSize={220}
-              columnOptions={defaultColumnOptions}
-              {...playerTableData}
-            />
-          )}
-        </Grid>
-      </Grid>
+      <Box typography="h4" sx={{ textAlign: 'center' }}>Draft</Box>
+      <Divider />
+      <Box sx={{ width: '100%' }}>
+        <MuiTable 
+          title="Owner Draft Info"
+          defaultPageSize={10}
+          {...ownerDraftTableData} />
+      </Box>
+      <Divider />
+      <Box sx={{ width: '49%', display: 'inline-block', pr: 1 }}>
+        <MuiTable
+          title="Draft"
+          defaultPageSize={220}
+          columnOptions={defaultColumnOptions}
+          {...draftTableData}
+        />
+      </Box>
+      <Box sx={{ width: '49%', display: 'inline-block', pl: 1 }}>
+        <MuiTable
+          title="Player Pool"
+          defaultPageSize={220}
+          columnOptions={defaultColumnOptions}
+          {...playerTableData}
+        />
+      </Box>
     </Box>
   )
 }
